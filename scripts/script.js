64b1b29e59e5a7cff1412e1c589f46f96a6245ed
Thabo Moloi <thabomoloicodes@gmail.com>
2022-07-21 05:09:56 +0200

    Remove old grid and calculate dimensions of the new divs

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,20 @@
 const grid = document.querySelector(".divgrid");
 
 function createGrid(x) {
+    // remove rows if any
+    const rows = document.querySelectorAll(".gridrow");
+    rows.forEach((row) => {
+        grid.removeChild(row);
+    });
+    // create new divs
     for (let i = 0; i < x; i++) {
         const gridRow = document.createElement("div");
         gridRow.className = "gridrow";
         for (let j = 0; j < x; j++) {
             const gridCell = document.createElement("div");
             gridCell.className = "gridcell";
-            gridCell.innerText = "t"
+            gridCell.style.width = `${grid.clientWidth / x}px`;
+            gridCell.style.height = `${grid.clientHeight / x}px`;
             gridRow.appendChild(gridCell);
         }
         grid.appendChild(gridRow);
