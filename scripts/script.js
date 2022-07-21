96f66a78669553c628433d80a211b0ad040b9a84
Thabo Moloi <thabomoloicodes@gmail.com>
2022-07-21 07:46:26 +0200

    Add event listeners for range, clear and hide grid buttons

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,9 @@ const darkenModeBtn = document.querySelector("#darken-mode");
 const eraserModeBtn = document.querySelector("#eraser-mode");
 // extra
 const range = document.querySelector("#range");
-const gridSize = document.querySelector(".grid-size");
+const gridSizes = document.querySelectorAll(".grid-size");
+gridSizes.forEach((size) => size.innerText = `${range.value}`);
+
 const clearBtn = document.querySelector("#clear");
 const hideGridBtn = document.querySelector("#hide-grid");
 // grid
@@ -34,7 +36,7 @@ function createGrid(x) {
         grid.appendChild(gridRow);
     }
 }
-
+createGrid(range.value);
 var mode;
 //============= EVENT LISTENERS ================
 const modeButtons = document.querySelectorAll(".modes button");
@@ -43,6 +45,7 @@ function removeActive() {
         button.className = "";
     })
 }
+// buttons
 colorModeBtn.addEventListener('click', function () {
     removeActive()
     this.className = "active";
@@ -67,4 +70,22 @@ eraserModeBtn.addEventListener('click', function () {
     removeActive()
     this.className = "active";
     mode = "eraser";
+});
+// range
+range.addEventListener('input', () => {
+    gridSizes.forEach((size) => {
+        size.innerText = `${range.value}`;
+        createGrid(range.value);
+    });
+});
+// clear
+clearBtn.addEventListener('click', () => {
+    createGrid(range.value);
+});
+// hide grid 
+hideGridBtn.addEventListener('click', () => {
+    const cells = document.querySelectorAll(".gridcell");
+    cells.forEach((cell) => {
+        cell.style.border = "none";
+    });
 });
